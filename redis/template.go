14d4650ee706a46b879bee744fa221fce806a1df
naoto67 <uver.com67@gmail.com>
2019-08-15 19:36:48 +0900

    add list data

--- redis/template.go
+++ redis/template.go
@@ -50,6 +50,45 @@ func setDataToCache(key string, data []byte) error {
 	return nil
 }
 
+func getListDataFromCache(key string) ([]byte, error) {
+	conn, err := redis.Dial("tcp", fmt.Sprintf("%s:%s", redisHost, redisPort))
+	if err != nil {
+		return err
+	}
+
+	data, err := redis.Bytes(conn.Do("LANGE", key, 0, -1))
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+// RPUSHは最後に追加
+func pushListDataToCache(key string, data []byte) error {
+	conn, err := redis.Dial("tcp", fmt.Sprintf("%s:%s", redisHost, redisPort))
+	if err != nil {
+		return err
+	}
+	err = conn.Do("RPUSH", key, data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// マッチするものを1つ削除
+func removeListDataFromCache(key string, data []byte) error {
+	conn, err := redis.Dial("tcp", fmt.Sprintf("%s:%s", redisHost, redisPort))
+	if err != nil {
+		return err
+	}
+	err = conn.Do("LREM", key, 1, data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func makeKey(id int64) string {
 	ID := strconv.Itoa(int(id))
 	return Key + ID
